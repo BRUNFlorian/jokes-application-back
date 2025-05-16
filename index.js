bf5241c09fb172e2379214c3b6833eb824bbebf3
Florian BRUN <flo33.brun@hotmail.fr>
2025-05-16 21:42:37 +0200

    Add CORS support

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 require("dotenv").config();
 
 const express = require("express");
+const cors = require("cors");
 const jokeRoutes = require("./routes/jokeRoutes");
 const app = express();
 const port = 3000;
+
+app.use(cors());
 
 app.use(express.json());
 
