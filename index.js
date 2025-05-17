dd2e81e920cc4f24bd804a909aee1519b9e8f956
Florian BRUN <flo33.brun@hotmail.fr>
2025-05-17 19:45:46 +0200

    New adjustements to fix the issue

--- index.js
+++ index.js
@@ -1,20 +1,14 @@
-require("dotenv").config(); // Charge les variables d'environnement depuis .env
-
 const express = require("express");
 const cors = require("cors");
 const jokeRoutes = require("./routes/jokeRoutes");
 const swaggerUi = require("swagger-ui-express");
 const swaggerJsdoc = require("swagger-jsdoc");
-
 const app = express();
-
-// Utilisation du port défini par Render ou d'un port par défaut
-const port = process.env.PORT || 3000; // Utilise process.env.PORT pour Render
+const port = process.env.PORT || 10000;
 
 app.use(cors());
-app.use(express.json());
+app.use(express.json()); // Assure-toi que le serveur accepte les requêtes JSON
 
-// Configuration de Swagger pour la documentation de l'API
 const swaggerOptions = {
   definition: {
     openapi: "3.0.0",
@@ -30,21 +24,17 @@ const swaggerOptions = {
       },
     ],
   },
-  apis: ["./routes/*.js"], // Spécifie où trouver les définitions de l'API
+  apis: ["./routes/*.js"], // chemin de tes fichiers de route
 };
 
 const swaggerSpec = swaggerJsdoc(swaggerOptions);
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
-// Route de test pour vérifier si le serveur fonctionne
-app.get("/", (req, res) => {
-  res.send("Le serveur Express fonctionne !");
-});
+// Routes principales
+app.get("/", (req, res) => res.send("Le serveur Express fonctionne !"));
+app.use("/api/v1", jokeRoutes); // Intégration des routes
 
-// Intégration des routes pour gérer les blagues
-app.use("/api/v1", jokeRoutes);
-
-// Lancer le serveur Express sur le port défini
+// Lancer le serveur Express
 app.listen(port, () => {
   console.log(`Serveur démarré sur le port ${port}`);
 });
