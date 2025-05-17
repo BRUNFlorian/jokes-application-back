951f7f1c64ac1af93cbbc59d15813319ed7f6423
Florian BRUN <flo33.brun@hotmail.fr>
2025-05-17 13:26:55 +0200

    Added Swagger integration to the backend for API documentation.

--- index.js
+++ index.js
@@ -3,12 +3,35 @@ require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const jokeRoutes = require("./routes/jokeRoutes");
+const swaggerUi = require("swagger-ui-express");
+const swaggerJsdoc = require("swagger-jsdoc");
+
 const app = express();
 const port = 3000;
 
 app.use(cors());
-
 app.use(express.json());
+
+const swaggerOptions = {
+  definition: {
+    openapi: "3.0.0",
+    info: {
+      title: "API de Blagues",
+      version: "1.0.0",
+      description: "Une API pour gérer des blagues façon Caramb*r",
+    },
+    servers: [
+      {
+        url: "http://localhost:3000/api/v1",
+        description: "Serveur local de développement",
+      },
+    ],
+  },
+  apis: ["./routes/*.js"],
+};
+
+const swaggerSpec = swaggerJsdoc(swaggerOptions);
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
 // Route de test
 app.get("/", (req, res) => {
--- routes/jokeRoutes.js
+++ routes/jokeRoutes.js
@@ -4,12 +4,48 @@ const router = express.Router();
 
 const jokeController = require("../controllers/jokeController");
 
+/**
+ * @swagger
+ * /jokes:
+ *   get:
+ *     summary: Récupère toutes les blagues
+ *     responses:
+ *       200:
+ *         description: Liste des blagues
+ */
 router.get("/jokes", jokeController.getAllJokes);
 
+/**
+ * @swagger
+ * /jokes/random:
+ *   get:
+ *     summary: Récupère une blague aléatoirement
+ *     responses:
+ *       200:
+ *         description: Une blague aléatoire
+ */
 router.get("/jokes/random", jokeController.getRandomJoke);
 
+/**
+ * @swagger
+ * /jokes/{id}:
+ *   get:
+ *     summary: Récupère une blague par son id
+ *     responses:
+ *       200:
+ *         description: Blague sélectionnée par l'id
+ */
 router.get("/jokes/:id", jokeController.getJokeById);
 
+/**
+ * @swagger
+ * /jokes:
+ *   post:
+ *     summary: Crée une nouvelle blague
+ *     responses:
+ *       200:
+ *         description: Création d'une blague
+ */
 router.post("/jokes", jokeController.addJoke);
 
 module.exports = router;
